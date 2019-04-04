246407b54c5484396c7f8b015364b9e9c7f7f498
Brian Blosser <bpb54321@gmail.com>
2019-04-04 13:10:36 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const React = window.React;
+
+const title = React.createElement(
+  'h1',
+  {
+    id: 'main-title',
+    title: 'This is a title.',
+  },
+  'My First React Element!'
+);
+
+console.log(title);
