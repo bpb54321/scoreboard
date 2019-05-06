51d73121a02f830bbe21c85873b3eb377b9bfb2f
Brian Blosser <bpb54321@gmail.com>
2019-05-06 16:18:42 -0400

    Passing down a changePlayerScore function from the App so the players array can be updated at the top level.

--- app.js
+++ app.js
@@ -1,24 +1,6 @@
 const React = window.React;
 const ReactDOM = window.ReactDOM;
 
-let players = [
-  {
-    name: "Brian",
-    score: 25,
-    id: 1,
-  },
-  {
-    name: "Laura",
-    score: 30,
-    id: 2,
-  },
-  {
-    name: "Zippy",
-    score: 35,
-    id: 3,
-  },
-];
-
 class Header extends React.Component {
   render() {
     return (
@@ -31,48 +13,108 @@ class Header extends React.Component {
 }
 
 class Counter extends React.Component {
+
   render() {
     return (
       <div className={"counter"}>
-        <button className={"counter-action decrement"}>-</button>
+        <button
+          className={"counter-action decrement"}
+          onClick={ () => this.props.changePlayerScore(this.props.playerId, 'decrement') }
+        >-</button>
         <span className={"counter-score"}>{this.props.score}</span>
-        <button className={"counter-action increment"}>+</button>
+        <button
+          className={"counter-action increment"}
+          onClick={ () => this.props.changePlayerScore(this.props.playerId, 'increment') }
+        >+</button>
       </div>
     );
   }
 }
 
 class Player extends React.Component {
+
   render() {
     return (
       <div className={"player"}>
-      <span className={"player-name"}>
-        {this.props.name}
-      </span>
+        <span className={"player-name"}>
+          {this.props.name}
+        </span>
         <Counter
+          changePlayerScore={this.props.changePlayerScore}
+          playerId={this.props.id}
           score={this.props.score}
         />
       </div>
     );
   }
+
 }
 
 class App extends React.Component {
+  state = {
+    players: [
+      {
+        name: "Brian",
+        score: 0,
+        id: 1,
+      },
+      {
+        name: "Laura",
+        score: 0,
+        id: 2,
+      },
+      {
+        name: "Zippy",
+        score: 0,
+        id: 3,
+      },
+    ],
+  };
+
+
+  changePlayerScore = (playerId, action) => {
+
+    this.setState((previousState) => {
+
+      let updatedPlayers = previousState.players.map((player) => {
+
+        if (player.id === playerId) {
+          if (action === 'increment') {
+            player.score++;
+          } else {
+            player.score--;
+          }
+        }
+
+        return player;
+
+      });
+
+      return {
+        players: updatedPlayers
+      };
+
+    });
+
+  };
+
   render() {
     return (
       <div className={"scoreboard"}>
         <Header
           title={"My Scoreboard"}
-          numberOfPlayers={this.props.initialPlayers.length}
+          numberOfPlayers={this.state.players.length}
         />
         {
-          this.props.initialPlayers.map(
+          this.state.players.map(
             (player) => {
               return (
                 <Player
                   name={player.name}
                   score={player.score}
-                  key={player.id.toString()}
+                  key={player.id.toString()} // React's required key prop must be a string
+                  id={player.id}
+                  changePlayerScore={this.changePlayerScore}
                 />
               );
             }
@@ -84,8 +126,6 @@ class App extends React.Component {
 }
 
 ReactDOM.render(
-  <App
-    initialPlayers={players}
-  />,
+  <App/>,
   document.getElementById('root')
 );
