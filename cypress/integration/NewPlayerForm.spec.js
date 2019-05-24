32c9103d4eb2581c7b74382cb5a4d096a1129e00
Brian Blosser <bpb54321@gmail.com>
2019-05-24 12:17:47 -0400

    Satisfied a test that said the font size should be in rems.

--- cypress/integration/NewPlayerForm.spec.js
+++ cypress/integration/NewPlayerForm.spec.js
@@ -25,6 +25,22 @@ describe('NewPlayerForm', function() {
   it('The add new player button has certain styles', function() {
     cy.get('@new-player-form')
       .find('[data-testid="add-new-player-button"]')
-      .should('have.css', 'font-family', 'Arial, sans-serif');
+      .should('have.css', 'font-family', 'Arial, sans-serif')
+      .should('have.css', 'background-color', 'rgb(68, 51, 141)')
+      .should('have.css', 'color', 'rgb(255, 255, 255)')
+      .should('have.css', 'font-size', '9.6px');
+  });
+
+  it('The add new player button\'s font-size doubles when the root font' +
+    ' size doubles', function () {
+
+    cy.get('html').then(function ($html) {
+      $html.css('font-size', '32px');
+      debugger;
+    });
+
+    cy.get('@new-player-form')
+      .find('[data-testid="add-new-player-button"]')
+      .should('have.css', 'font-size', '19.2px')
   });
 });
