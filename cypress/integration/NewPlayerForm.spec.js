4778b27bcd824bec2fa402e5842552745076c454
Brian Blosser <bpb54321@gmail.com>
2019-05-23 21:09:29 -0400

    Added a button for adding a new player.

--- cypress/integration/NewPlayerForm.spec.js
+++ cypress/integration/NewPlayerForm.spec.js
@@ -1,8 +1,18 @@
 describe('NewPlayerForm', function() {
-  it('Has a button for adding a new player', function() {
-    cy.get('[data-testid="new-player-form"]').as('new-player-form');
 
+  beforeEach(function() {
+    // Get the wrapper element for this component
+    cy.get('[data-testid="new-player-form"]').as('new-player-form');
+  });
+
+  it('Is an HTML form',function() {
     cy.get('@new-player-form')
-      .find('[data-testid="add-new-player-button"]');
+      .should('match', 'form');
+  });
+
+  it('Has a button for adding a new player', function() {
+    cy.get('@new-player-form')
+      .find('[data-testid="add-new-player-button"]')
+      .should('match', 'button');
   });
 });
--- src/components/NewPlayerForm.js
+++ src/components/NewPlayerForm.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default class NewPlayerForm extends React.Component {
   render() {
     return (
-      <div data-testid="add-new-player-form"></div>
+      <form data-testid="new-player-form">
+        <button data-testid="add-new-player-button">Add Player</button>
+      </form>
     );
   }
 }
